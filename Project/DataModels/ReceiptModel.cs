4bfb9ed699c6d4f90bf0200da6b0d0118f9b87d6
rowin04 <116190178+rowin04@users.noreply.github.com>
2024-11-11 13:24:12 +0100

    Oli _changes Reservation - user gets option for hot seat with extra cost - user gets receipt when finishing order

---
+++ Project/DataModels/ReceiptModel.cs
@@ -0,0 +1,35 @@
+using System.Text.Json.Serialization;
+
+
+public class ReceiptModel
+{
+
+    [JsonPropertyName("id")]
+    public int Id { get; set; }
+
+    [JsonPropertyName("reservationid")]
+    public int ReservationId { get; set; }
+
+    [JsonPropertyName("clientid")]
+    public int ClientId { get; set; }
+
+    [JsonPropertyName("cost")]
+    public int Cost { get; set; }
+
+    [JsonPropertyName("date")]
+    public DateTime Date { get; set; }
+
+    
+    public ReceiptModel(int id, int reservationId, int clientId, int cost, DateTime date)
+    {
+        Id = id;
+        ReservationId = reservationId;
+        ClientId = clientId;
+        Cost = cost;
+        Date = date;
+    }
+}
+
+
+
+
